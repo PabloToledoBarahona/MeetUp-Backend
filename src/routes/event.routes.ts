093dd834507933da7e2a974bde139d8fb9ebf213
Pablo Toledo <pali.toledo.b@gmail.com>
2025-04-28 11:19:28 -0400

    getEventById agregado

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { createEvent, editEvent, getUserEvents, removeEvent, getBirthdayTemplate, getJunteTemplate, createEventFromTemplate, cancelEventController } from '../controllers/event.controller'
+import { createEvent, editEvent, getUserEvents, removeEvent, getBirthdayTemplate, getJunteTemplate, createEventFromTemplate, cancelEventController, getEventById } from '../controllers/event.controller'
 import { authenticateToken } from '../middlewares/auth.middleware'
 
 const router = Router()
 
 router.post('/', authenticateToken, createEvent)
 router.get('/', authenticateToken, getUserEvents)
+router.get('/:id', authenticateToken, getEventById)
 router.put('/:id', authenticateToken, editEvent)
 router.delete('/:id', authenticateToken, removeEvent)
 router.get('/templates/birthday', authenticateToken, getBirthdayTemplate)
