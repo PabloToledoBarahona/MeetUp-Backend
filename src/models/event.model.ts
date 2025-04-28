19eda947fe44977c4a46a6bbfe5cfd26dfc24d92
Pablo Toledo <pali.toledo.b@gmail.com>
2025-04-28 11:27:38 -0400

    Se agregó el campo de imageUrl, se hizo opcional el campo de endTime

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -6,7 +6,8 @@ export interface IEvent extends Document {
   location: string
   category: string
   startTime: Date
-  endTime: Date
+  endTime?: Date 
+  imageUrl?: string 
   createdBy: Schema.Types.ObjectId
   isCancelled: boolean
   budget?: number
@@ -19,10 +20,11 @@ const eventSchema = new Schema<IEvent>(
     location: { type: String, required: true },
     category: { type: String, required: true },
     startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true },
+    endTime: { type: Date }, 
+    imageUrl: { type: String },
     createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     isCancelled: { type: Boolean, default: false },
-    budget: { type: Number } // 🔥
+    budget: { type: Number }
   },
   { timestamps: true }
 )
